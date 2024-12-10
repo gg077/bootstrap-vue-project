4a9c0589c7b17dc1f5456195ff7d32a4de1e0003
JanVDB2000 <janvandenbogaert2000@gmail.com>
2024-12-10 15:48:01 +0100

    rrrr

--- src/components/GptOefeningen.vue
+++ src/components/GptOefeningen.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="container mt-5 text-center">
-    <h2>Countdown Timer</h2>
-    <p>Tijd: <strong>{{ timer }}</strong></p>
-    <button @click="startCountdown" class="btn btn-success">Start</button>
+  <div class="container mt-5">
+    <h2>Mijn Lijst</h2>
+    <input v-model="listitem" type="text" class="form-control mb-2" placeholder="Voeg een item toe..."/>
+    <button @click="voegtoe" class="btn btn-success mb-3">Toevoegen</button>
+    <ul class="list-group">
+      <li v-for="(item, index) in items" :key="index" class="list-group-item">{{item}}</li>
+    </ul>
   </div>
 </template>
 <script>
 import { ref } from 'vue';
 export default {
   setup() {
-    const timer = ref(10);
-    let interval;
-    const startCountdown = () => {
-      interval = setInterval(() => {
-        if (timer.value > 0) {
-          timer.value--;
-        } else {
-          clearInterval(interval);
-        }
-      }, 1000);
-    };
-    return { timer, startCountdown };
+    const listitem = ref('');
+    const items = ref([]);
+    const voegtoe = () =>{
+      items.value.push(listitem.value.trim());
+    }
+    return {listitem, items, voegtoe}
   },
 };
 </script>
